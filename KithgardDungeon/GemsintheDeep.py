c682674da4c3e2814d40150952e5df61644718de
Inquis1t0r <pi0tr3k9292@gmail.com>
2019-08-28 12:34:05 +0200

    Create GemsintheDeep.py

---
+++ KithgardDungeon/GemsintheDeep.py
@@ -0,0 +1,11 @@
+# Zdobądź wszystkie klejnoty używając komend ruchu.
+
+hero.moveRight()
+hero.moveDown()
+hero.moveUp()
+hero.moveUp()
+hero.moveRight()
+hero.moveDown()
+hero.moveUp()
+hero.moveRight()
+
