d162750f80064fd4d0719177040b1e23d7940d08
Inquis1t0r <pi0tr3k9292@gmail.com>
2019-09-04 14:25:34 +0200

    Create ThePrisoner.py

---
+++ KithgardDungeon/ThePrisoner.py
@@ -0,0 +1,16 @@
+# Free the prisoner, defeat the guard and grab the gem.
+
+# Free Patrick from behind the "Weak Door".
+hero.moveRight()
+hero.attack("Weak Door")
+# Defeat the guard, named "Two".
+hero.moveRight(2)
+hero.moveDown(2)
+hero.attack("Two")
+hero.attack("Two")
+hero.moveRight()
+# Get the gem.
+hero.moveDown()
+hero.moveRight
+
+
